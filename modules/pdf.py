68dde4880179bda18779edae6ba7abd13c67b593
Marco Andronaco <andronacomarco@gmail.com>
2025-12-15 13:06:28 +0100

    Add magazines

--- modules/pdf.py
+++ modules/pdf.py
@@ -5,7 +5,7 @@ from io import BytesIO
 
 def get_title_from_filename(filename: Path) -> str:
     """Extract title from filename by removing extensions and replacing underscores with spaces
-       e.g. corriere-della-sera_20240615.temp.pdf -> Corriere della sera - 15/06/2024
+       e.g. corriere-della-sera-1_20240615.temp.pdf -> Corriere Della Sera - 15/06/2024
     """
     
     base = filename.stem.replace(".temp", "")
@@ -21,6 +21,13 @@ def get_title_from_filename(filename: Path) -> str:
         formatted_date = date_obj.strftime("%d/%m/%Y")
     except ValueError:
         formatted_date = date_part
+
+    # if name part ends with "-\d+", remove that part
+    if '-' in name_part:
+        split_result = name_part.rsplit('-', 1)
+        possible_number = split_result[-1]
+        if possible_number.isdigit():
+            name_part = split_result[0]
 
     title = f"{name_part.replace('-', ' ').title()} - {formatted_date}"
     return title
